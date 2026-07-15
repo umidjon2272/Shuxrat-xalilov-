eec8c570bca1ea40ecb9e372a2f50432390590b8
umidjon2272 <umidjonjurabayev06@gmail.com>
2026-07-15 19:26:53 +0500

    shamshoda

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/global.scss'
 
-// Meta Pixel
-(function(f,b,e,v,n,t,s){
-  if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)
-})(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
-window.fbq('init', '972192929225708');
-window.fbq('track', 'PageView');
-
 document.documentElement.setAttribute('data-theme', 'light');
+
+// Meta Pixel
+const script = document.createElement('script');
+script.async = true;
+script.src = 'https://connect.facebook.net/en_US/fbevents.js';
+script.onload = () => {
+  window.fbq('init', '972192929225708');
+  window.fbq('track', 'PageView');
+};
+document.head.appendChild(script);
+
+window._fbq = window.fbq = function() {
+  if (window.fbq.callMethod) {
+    window.fbq.callMethod.apply(window.fbq, arguments);
+  } else {
+    window.fbq.queue.push(arguments);
+  }
+};
+window.fbq.push = window.fbq;
+window.fbq.loaded = true;
+window.fbq.version = '2.0';
+window.fbq.queue = [];
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
